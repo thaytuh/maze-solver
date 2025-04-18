bf2d718a8322b03c84e5813d948c5c65844322f1
thaytuh <htaylorkrass@gmail.com>
2025-04-17 17:13:44 -0700

    modified cell class for better readability and functionality and modified main with better tests

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    cell = Cell(40, 40, 80, 80, win)
-    cell.has_right_wall = False
-    cell.draw()
-    cell2 = Cell(80, 40, 120, 80, win)
-    cell2.has_left_wall = False
-    cell2.draw()
+    
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+    
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+    
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+    
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+    
     win.wait_for_close()
 
 
