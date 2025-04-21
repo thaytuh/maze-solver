fa14a4f176501a0ebe1dd7331aee8de2e8078d17
thaytuh <htaylorkrass@gmail.com>
2025-04-21 00:13:43 -0700

    added function to draw line from middle of cell to middle of other cell and added testing in main

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ from cell import Cell
 def main():
     win = Window(800, 600)
     
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    cell1 = Cell(win)
+    cell1.has_left_wall = False
+    cell1.draw(50, 50, 100, 100)
     
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    cell2 = Cell(win)
+    cell2.has_right_wall = False
+    cell2.draw(125, 125, 200, 200)
     
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    cell1.draw_move(cell2)
     
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    cell3 = Cell(win)
+    cell3.has_bottom_wall = False
+    cell3.draw(225, 225, 250, 250)
+    
+    cell4 = Cell(win)
+    cell4.has_top_wall = False
+    cell4.draw(300, 300, 500, 500)
+    
+    cell3.draw_move(cell4, undo=True)
     
     win.wait_for_close()
 
