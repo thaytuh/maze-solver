eb061c86ef7ae2c2ac8ee6b66bfb1c34d59907bd
thaytuh <htaylorkrass@gmail.com>
2025-05-14 13:15:41 -0700

    added method to break entrance and exit lines like a real maze

--- tests.py
+++ tests.py
@@ -27,6 +27,27 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+        
+    def test_break_entrance_and_exit(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+        self.assertEqual(
+            m1._cells[0][0].has_top_wall,
+            False
+        )
+        self.assertEqual(
+            m1._cells[m1._num_cols - 1][m1._num_rows - 1].has_bottom_wall,
+            False
+        )
 
 if __name__ == "__main__":
     unittest.main()
