3c4d576ac0a2c3f949877dd3988c59d7636b7ae0
thaytuh <htaylorkrass@gmail.com>
2025-05-14 14:11:25 -0700

    added recursive method to break cell walls randomly, added method to reset status of visited cells, added tests

--- maze.py
+++ maze.py
@@ -11,7 +11,8 @@ class Maze:
             num_cols,
             cell_size_x,
             cell_size_y,
-            win=None
+            win=None,
+            seed=None
         ):
         self._cells = []
         self._x1 = x1
@@ -21,9 +22,13 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed)
         
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        self._reset_cells_visited()
     
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -56,3 +61,45 @@ class Maze:
         self._draw_cell(0,0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+    
+    def _break_walls_r(self, i, j):
+        current_cell = self._cells[i][j]
+        current_cell.visited = True
+        
+        while True:
+            unvisited_neighbors = []
+            if j > 0 and not self._cells[i][j - 1].visited:
+                unvisited_neighbors.append((i, j - 1, "up"))
+            if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
+                unvisited_neighbors.append((i + 1, j, "right"))
+            if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
+                unvisited_neighbors.append((i, j + 1, "down"))
+            if i > 0 and not self._cells[i - 1][j].visited:
+                unvisited_neighbors.append((i - 1, j, "left"))
+            
+            if len(unvisited_neighbors) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            next_cell_info = random.randrange(len(unvisited_neighbors))
+            next_i, next_j, direction = unvisited_neighbors[next_cell_info]
+            
+            if direction == "up":
+                current_cell.has_top_wall = False
+                self._cells[next_i][next_j].has_bottom_wall = False
+            if direction == "right":
+                current_cell.has_right_wall = False
+                self._cells[next_i][next_j].has_left_wall = False
+            if direction == "down":
+                current_cell.has_bottom_wall = False
+                self._cells[next_i][next_j].has_top_wall = False
+            if direction == "left":
+                current_cell.has_left_wall = False
+                self._cells[next_i][next_j].has_right_wall = False
+            
+            self._break_walls_r(next_i, next_j)
+        
+    def _reset_cells_visited(self):
+        for col in self._cells:
+            for cell in col:
+                cell.visited = False
--- tests.py
+++ tests.py
@@ -29,8 +29,8 @@ class Tests(unittest.TestCase):
         )
         
     def test_break_entrance_and_exit(self):
-        num_cols = 16
-        num_rows = 12
+        num_cols = 12
+        num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
@@ -48,6 +48,17 @@ class Tests(unittest.TestCase):
             m1._cells[m1._num_cols - 1][m1._num_rows - 1].has_bottom_wall,
             False
         )
+    
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False
+                )
 
 if __name__ == "__main__":
     unittest.main()
